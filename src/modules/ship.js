a07d36f565d341867fb432083b6fe8abce6ffb26
davide <davidebellucci975@gmail.com>
2022-04-18 18:19:39 +0200

    add class Ship

---
+++ src/modules/ship.js
@@ -0,0 +1,17 @@
+class Ship {
+  constructor(length) {
+    this.coordinate = {};
+    this.direction = "";
+    this.body = [];
+
+    for (let i = 0; i < length; i++) {
+      this.body.push("");
+    }
+  }
+
+  isSunk() {
+    return this.body.every((element) => element.status === "hit");
+  }
+}
+
+export default Ship;
