8dc0b404f52412a6ae60cd093d28ec72eef2a7c4
davide <davidebellucci975@gmail.com>
2022-04-25 10:42:04 +0200

    add new status hitAndOccupied for hit ships

--- src/modules/square.js
+++ src/modules/square.js
@@ -1,10 +1,15 @@
 class Square {
   static id = 0;
-  static status = { empty: "empty", occupied: "occupied", hit: "hit" };
+  static status = {
+    empty: "empty",
+    occupied: "occupied",
+    hit: "hit",
+    hitAndOccupied: "hitAndOccupied",
+  };
 
   constructor() {
     this.status = Square.status.empty;
-    this.id = ++Square.id;
+    this.id = Square.id++;
   }
 
   setStatusOccupied() {
@@ -13,6 +18,10 @@ class Square {
 
   setStatusHit() {
     this.status = Square.status.hit;
+  }
+
+  setStatusHitAndOccupied() {
+    this.status = Square.status.hitAndOccupied;
   }
 }
 
