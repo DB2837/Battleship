00b4b2aae37e1a94e877d1f3f70a9a092671540d
davide <davidebellucci975@gmail.com>
2022-04-25 10:43:30 +0200

    fix attack method to properly convert DOMboard square id into coordinate

--- src/modules/player.js
+++ src/modules/player.js
@@ -16,7 +16,32 @@ class Player {
 
   attack(gameboard, coordinate) {
     if (this.playerType === "human" && this.isMyTurn) {
+      this.switchTurn();
       gameboard.reciveAttack(coordinate);
+      return;
+    }
+
+    if (this.playerType === "AI" && this.isMyTurn) {
+      this.switchTurn();
+
+      const randomSquare = Math.floor(
+        Math.random() * (this.freeSquares.length - 1)
+      );
+
+      const selectedSquare = this.freeSquares.splice(randomSquare, 1);
+
+      if (selectedSquare[0].id > 9) {
+        const x = +selectedSquare[0].id.toString()[0];
+        const y = +selectedSquare[0].id.toString()[1];
+
+        gameboard.reciveAttack({ x, y });
+      } else {
+        const x = 0;
+        const y = +selectedSquare[0].id.toString()[0];
+
+        gameboard.reciveAttack({ x, y });
+      }
+
       return;
     }
   }
