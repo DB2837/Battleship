da8556d1148029ceaa21b75577032580b5f78234
davide <davidebellucci975@gmail.com>
2022-04-30 18:58:33 +0200

    add start game and inizialize board methods

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -1,4 +1,5 @@
 import Square from "./square";
+import Ship from "./ship";
 
 class UI {
   static boardCounter = 0;
@@ -19,7 +20,7 @@ class UI {
     for (let i = 0; i < length; i++) {
       const square = UI.generateSquare();
       square.dataset.index = i;
-      square.style.backgroundColor = "green";
+      square.style.backgroundColor = "mediumspringgreen";
       ship.appendChild(square);
     }
 
@@ -68,7 +69,13 @@ class UI {
         if (gameboard.board[i][j].status === "occupied" && gameboard.id === 0) {
           DOMboard.children[
             +[i.toString() + j.toString()]
-          ].style.backgroundColor = "green";
+          ].style.backgroundColor = "mediumspringgreen";
+        }
+
+        if (gameboard.board[i][j].status === "empty") {
+          DOMboard.children[
+            +[i.toString() + j.toString()]
+          ].style.backgroundColor = "white";
         }
 
         if (gameboard.board[i][j].status === "hit") {
@@ -83,6 +90,82 @@ class UI {
         }
       }
     }
+  }
+
+  static emptyBoard(gameboard, DOMboard) {
+    for (let i = 0; i < gameboard.dimension; i++) {
+      for (let j = 0; j < gameboard.dimension; j++) {
+        gameboard.board[i][j].status = "empty";
+
+        delete DOMboard.children[+[i.toString() + j.toString()]].dataset
+          .isEmpty;
+      }
+    }
+  }
+
+  static inizializeShipContainer(shipsArray, container) {
+    for (let i = 0; i < container.children.length; i++) {
+      container.children[i].remove();
+    }
+
+    for (let i = 0; i < shipsArray.length; i++) {
+      container.appendChild(shipsArray[i]);
+    }
+  }
+
+  static initializeBoard(playerBoard, playerDOMboard) {
+    UI.emptyBoard(playerBoard, playerDOMboard);
+
+    const myShip1 = new Ship(2);
+    const myShip2 = new Ship(3);
+    const myShip3 = new Ship(3);
+    const myShip4 = new Ship(4);
+    const myShip5 = new Ship(5);
+
+    const myShips = [myShip1, myShip2, myShip3, myShip4, myShip5];
+
+    playerBoard.placeRandomFleet(myShips);
+    UI.renderVisualEffects(playerBoard, playerDOMboard);
+  }
+
+  static startNewGame(
+    gameboardOne,
+    gameboardTwo,
+    gridPlayerOne,
+    gridPlayerTwo,
+    shipsContainer
+  ) {
+    gameboardOne.emptyShipContainer();
+    gameboardTwo.emptyShipContainer();
+
+    gridPlayerTwo.style.display = "none";
+    UI.emptyBoard(gameboardOne, gridPlayerOne);
+    UI.emptyBoard(gameboardTwo, gridPlayerTwo);
+    UI.renderVisualEffects(gameboardOne, gridPlayerOne);
+    UI.renderVisualEffects(gameboardTwo, gridPlayerTwo);
+    shipsContainer.parentElement.style.display = "grid";
+
+    for (let i = 0; i < shipsContainer.children.length; i++) {
+      shipsContainer.children[i].classList.remove("invisible");
+    }
+  }
+
+  static checkForWinner(gameboardOne, gameboardTwo, overlay) {
+    if (gameboardOne.shipContainer.length === 0) {
+      overlay.style.display = "flex";
+      overlay.querySelector("h2").textContent = "";
+      overlay.querySelector("h2").textContent = "You Lose!";
+
+      return true;
+    } else if (gameboardTwo.shipContainer.length === 0) {
+      overlay.style.display = "flex";
+      overlay.querySelector("h2").textContent = "";
+      overlay.querySelector("h2").textContent = "You Win!";
+
+      return true;
+    }
+
+    return false;
   }
 }
 
